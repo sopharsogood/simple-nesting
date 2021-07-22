ffb8de6070fa2db8baf967843c620d5e97fb0c24
sopharsogood <justanothersophie@gmail.com>
2021-07-21 20:34:17 -0400

    commit tree 57734dd03a2d8dd9154f2c0187d44681a9d35cfc

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -16,6 +15,7 @@ def hopper
         }
      }
 
+     programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +84,12 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+
+  programmer_hash
 end
 
 def changing_alan
@@ -99,8 +108,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+  #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+  alans_new_info = "GUI"
+  
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
      
      
 end
@@ -121,5 +133,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+  programmer_hash
 
 end
